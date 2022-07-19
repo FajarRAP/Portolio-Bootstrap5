58d2079ebf8f5e57aa4d1f552aa91ccb8a709bc5
FajarRAP <fajary781@gmail.com>
2022-07-19 13:36:29 +0700

    fixkan tombol

--- Form/setelahIsi.php
+++ Form/setelahIsi.php
@@ -1,4 +1,22 @@
-<?php
-echo "Terima kasih telah mengisi<br>";
-echo "<a href='index.php'>Kembali</a><br>";
-?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+  <?php
+    echo "Terima kasih telah mengisi<br>";
+    echo "<a href='index.php' class='btn btn-success'>Kembali</a><br>";
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
